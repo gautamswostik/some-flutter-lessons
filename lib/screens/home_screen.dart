62077f69e5709f116e5d6d8d7eab535991ef78c0
gautamswostik <swastikg11@gmail.com>
2022-04-20 16:53:18 +0545

    added translation for every text in app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fluuter_boilerplate/screens/app_theme/app_theme_screen.dart';
 import 'package:fluuter_boilerplate/screens/localization/app_language_screen.dart';
 import 'package:fluuter_boilerplate/utils/app_texts/app_texts.dart';
+import 'package:fluuter_boilerplate/utils/extensions/string_extensions.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key, required this.themeValue}) : super(key: key);
@@ -15,7 +16,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter Components'),
+        title: Text(AppTexts.mainTitle.translateTo(context)),
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -23,7 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
             Card(
               child: ListTile(
                 leading: Image.asset('assets/images/theme.png'),
-                title: const Text(AppTexts.darkMode),
+                title: Text(AppTexts.darkMode.translateTo(context)),
                 onTap: () {
                   Navigator.of(context).push(
                     MaterialPageRoute(
@@ -37,7 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
             Card(
               child: ListTile(
                 leading: Image.asset('assets/images/translation.png'),
-                title: const Text(AppTexts.localization),
+                title: Text(AppTexts.localization.translateTo(context)),
                 onTap: () {
                   Navigator.of(context).push(
                     MaterialPageRoute(
