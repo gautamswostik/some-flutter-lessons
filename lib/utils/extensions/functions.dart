87f96b8379afeccd0584e72da0e17cef241f840e
gautamswostik <swastikg11@gmail.com>
2022-05-02 21:47:14 +0545

    fixed multidex error

--- lib/utils/extensions/functions.dart
+++ lib/utils/extensions/functions.dart
@@ -1,10 +1,9 @@
 import 'package:url_launcher/url_launcher.dart';
 
 launchInBrowser(String url) async {
-  if (!await launch(
-    url,
-    forceSafariVC: false,
-    forceWebView: false,
+  if (!await launchUrl(
+    Uri.parse(url),
+    mode: LaunchMode.externalApplication,
   )) {
     throw 'Could not launch $url';
   }
