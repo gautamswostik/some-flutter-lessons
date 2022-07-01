0f96d5abcfa55abb2375050f7524e9a791779309
gautamswostik <swastikg11@gmail.com>
2022-07-01 15:36:50 +0545

    added .env for tokens

--- lib/app_setup/mapdata/map_data.dart
+++ lib/app_setup/mapdata/map_data.dart
@@ -1,9 +1,11 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class MapUrl {
   const MapUrl();
 
-  static const String baseUrl = 'https://api.mapbox.com/styles/v1/';
-  static const String acceesToken = '';
-  static const String templteUrl =
+  static String baseUrl = dotenv.get('BASEURL', fallback: '');
+  static String acceesToken = dotenv.get('MAPBOXTOKEN', fallback: '');
+  static String templteUrl =
       '$baseUrl/ckzxljoyd000u14pqy5u4dh5n/tiles/256/{z}/{x}/{y}@2x?access_token=$acceesToken';
   static const String id = 'mapbox.mapbox-streets-v8';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:fluuter_boilerplate/app/app.dart';
 import 'package:fluuter_boilerplate/app/bloc_observer.dart';
 import 'package:fluuter_boilerplate/app_setup/hive/hive_setup.dart';
@@ -9,6 +10,7 @@ import 'package:fluuter_boilerplate/app_setup/hive/hive_setup.dart';
 List<CameraDescription> cameras = [];
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: 'assets/env/.env');
   await HiveSetup.initHive();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   try {
