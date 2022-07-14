9e496308521ba83fc8208cfb41e0c7a44c219898
gautamswostik <swastikg11@gmail.com>
2022-07-14 07:54:45 +0545

    unit tested local note repository

--- test/repo_test/local_notes_repo_test.dart
+++ test/repo_test/local_notes_repo_test.dart
@@ -1,8 +1,6 @@
-import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fluuter_boilerplate/app_setup/hive/hive_box.dart';
 import 'package:fluuter_boilerplate/infrastructure/local_notes/local_notes_repo.dart';
-import 'package:fluuter_boilerplate/infrastructure/local_notes/note_adapter/note_entities.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:mockito/mockito.dart';
 import 'package:uuid/uuid.dart';
@@ -69,22 +67,159 @@ void main() {
       );
 
       test(
-        'Testing Get Local Note Success',
+        'Testing get Saved Local Data success',
         () async {
-          //arrange
+          //arange
           when(mockHiveInterface.isAdapterRegistered(1))
               .thenAnswer((_) => true);
           when(mockHiveInterface.openBox<MockLocalNoteEntity>(HiveBox.notesBox))
               .thenAnswer((_) async => mockBox);
-          when(mockUuid.v4()).thenAnswer((_) => 'uuid');
+          when(mockBox.values).thenAnswer((_) => <MockLocalNoteEntity>[]);
 
           //act
-          final data = await localNotesRepository.addNote(
-              localNoteEntity: mockLocalNoteEntity);
+          final data = await localNotesRepository.getLocalNotes();
+          //verify
           expect(data.isRight(), true);
-          //varify
-          verify(mockBox.put('uuid', mockLocalNoteEntity));
           verify(mockHiveInterface.openBox(HiveBox.notesBox));
+        },
+      );
+
+      test(
+        'Testing get Saved Local Data error',
+        () async {
+          //arange
+          when(mockHiveInterface.openBox<MockLocalNoteEntity>(HiveBox.notesBox))
+              .thenThrow(HiveError('error'));
+          when(mockHiveInterface.isAdapterRegistered(1))
+              .thenAnswer((_) => true);
+
+          try {
+            await localNotesRepository.getLocalNotes();
+          } catch (e) {
+            //verify
+            expect(e, isA<HiveError>());
+          }
+        },
+      );
+      test(
+        'Testing Edit Local Data success',
+        () async {
+          //arange
+          when(mockHiveInterface.isAdapterRegistered(1))
+              .thenAnswer((_) => true);
+          when(mockHiveInterface.openBox<MockLocalNoteEntity>(HiveBox.notesBox))
+              .thenAnswer((_) async => mockBox);
+          when(mockBox.containsKey('null')).thenReturn(true);
+
+          //act
+          final data = await localNotesRepository.editData(
+            key: 'null',
+            localNoteEntity: mockLocalNoteEntity,
+          );
+          //verify
+          expect(data.isRight(), true);
+          verify(mockHiveInterface.openBox(HiveBox.notesBox));
+        },
+      );
+
+      test(
+        'Testing edit Saved Local Data error',
+        () async {
+          //arange
+          when(mockHiveInterface.isAdapterRegistered(1))
+              .thenAnswer((_) => true);
+          when(mockHiveInterface.openBox<MockLocalNoteEntity>(HiveBox.notesBox))
+              .thenAnswer((_) async => mockBox);
+          when(mockBox.containsKey('null')).thenReturn(false);
+
+          //act
+          final data = await localNotesRepository.editData(
+            key: 'null',
+            localNoteEntity: mockLocalNoteEntity,
+          );
+          //verify
+          expect(data.isLeft(), true);
+          verify(mockHiveInterface.openBox(HiveBox.notesBox));
+        },
+      );
+
+      test(
+        'Testing edit Saved Local Data error',
+        () async {
+          //arange
+          when(mockHiveInterface.openBox<MockLocalNoteEntity>(HiveBox.notesBox))
+              .thenThrow(HiveError('error'));
+          when(mockHiveInterface.isAdapterRegistered(1))
+              .thenAnswer((_) => true);
+
+          try {
+            await localNotesRepository.editData(
+              key: 'null',
+              localNoteEntity: mockLocalNoteEntity,
+            );
+          } catch (e) {
+            //verify
+            expect(e, isA<HiveError>());
+          }
+        },
+      );
+      test(
+        'Testing delete Local Data success',
+        () async {
+          //arange
+          when(mockHiveInterface.isAdapterRegistered(1))
+              .thenAnswer((_) => true);
+          when(mockHiveInterface.openBox<MockLocalNoteEntity>(HiveBox.notesBox))
+              .thenAnswer((_) async => mockBox);
+          when(mockBox.containsKey('null')).thenReturn(true);
+
+          //act
+          final data = await localNotesRepository.deleteData(
+            key: 'null',
+          );
+          //verify
+          expect(data.isRight(), true);
+          verify(mockHiveInterface.openBox(HiveBox.notesBox));
+        },
+      );
+
+      test(
+        'Testing delete Saved Local Data error',
+        () async {
+          //arange
+          when(mockHiveInterface.isAdapterRegistered(1))
+              .thenAnswer((_) => true);
+          when(mockHiveInterface.openBox<MockLocalNoteEntity>(HiveBox.notesBox))
+              .thenAnswer((_) async => mockBox);
+          when(mockBox.containsKey('null')).thenReturn(false);
+
+          //act
+          final data = await localNotesRepository.deleteData(
+            key: 'null',
+          );
+          //verify
+          expect(data.isLeft(), true);
+          verify(mockHiveInterface.openBox(HiveBox.notesBox));
+        },
+      );
+
+      test(
+        'Testing delete Saved Local Data error',
+        () async {
+          //arange
+          when(mockHiveInterface.openBox<MockLocalNoteEntity>(HiveBox.notesBox))
+              .thenThrow(HiveError('error'));
+          when(mockHiveInterface.isAdapterRegistered(1))
+              .thenAnswer((_) => true);
+
+          try {
+            await localNotesRepository.deleteData(
+              key: 'null',
+            );
+          } catch (e) {
+            //verify
+            expect(e, isA<HiveError>());
+          }
         },
       );
     },
